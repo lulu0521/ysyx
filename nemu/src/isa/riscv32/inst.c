1656d0625d65beee0938a7a42b56215c7036dc8f
ysyx_22041071-majianlu <majianlu_0521@163.com>
2022-05-27 23:21:36 +0800

    dummy

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -9,7 +9,7 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_N, // none
+  TYPE_N, TYPE_J,// none
 };
 
 #define src1R(n) do { *src1 = R(n); } while (0)
@@ -21,7 +21,8 @@ enum {
 
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return BITS(i, 31, 12) << 12; }
-static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
+static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5 ) | BITS(i, 11, 7); }
+static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i,19,12)<<12) | (BITS(i,20,20)<<11) | (BITS(i,30,21)<<1);}
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -33,6 +34,7 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_J: src1I(immJ(i)); break;
   }
 }
 
@@ -48,11 +50,38 @@ static int decode_exec(Decode *s) {
 
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(dest) = src1);
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = s->pc + src1);
+
   INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(dest) = Mr(src1 + src2, 4));
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + src2);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(dest) = src1 + src2);
+
+
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + dest, 4, src2));
+
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = src1 + s->pc);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+/*80000000 <_start>:
+80000000:	00000413          	(ok)li	s0,0
+80000004:	00009117          	(ok)auipc	sp,0x9
+80000008:	ffc10113          	(ok)addi	sp,sp,-4 # 80009000 <_end>
+8000000c:	00c000ef          	(ok)jal	ra,80000018 <_trm_init>
+
+80000010 <main>:
+80000010:	00000513          	(ok)li	a0,0
+80000014:	00008067          	(ok)ret
+
+80000018 <_trm_init>:
+80000018:	80000537          	(ok)lui	a0,0x80000
+8000001c:	ff010113          	(ok)addi	sp,sp,-16
+80000020:	03850513          	(ok)addi	a0,a0,56 # 80000038 <_end+0xffff7038>
+80000024:	00112623          	(ok)sw	ra,12(sp)
+80000028:	fe9ff0ef          	(ok)jal	ra,80000010 <main>
+8000002c:	00050513          	mv	a0,a0
+80000030:	00100073          	(ok)ebreak
+80000034:	0000006f          	j	80000034 <_trm_init+0x1c>*/ 
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
