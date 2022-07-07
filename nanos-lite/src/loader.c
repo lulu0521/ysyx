c5a58740f7a60811e106345dd4f897244c5cb7ed
ysyx_22041071-majianlu <majianlu_0521@163.com>
2022-07-07 21:44:24 +0800

    first program

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,5 @@
 #include <proc.h>
 #include <elf.h>
-
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -9,9 +8,22 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr elf;
+  ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
+  assert( *(uint32_t *)elf.e_ident == 0x464c457f);
+  int i; 
+  for(i=0;i<elf.e_phnum;i++){
+    Elf_Phdr pro_h;
+    ramdisk_read(&pro_h,elf.e_phoff+i*elf.e_phentsize,elf.e_phentsize);
+    if(pro_h.p_type==PT_LOAD){
+      ramdisk_read((void *)pro_h.p_vaddr,pro_h.p_offset,pro_h.p_memsz);
+      memset((void *)(pro_h.p_vaddr+pro_h.p_filesz),0,pro_h.p_memsz-pro_h.p_filesz);
+    }
+  }
+  return (uintptr_t)elf.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -9,8 +9,10 @@ extern uint8_t ramdisk_end;
  * a physical one, which is necessary for a microkernel.
  */
 
+
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
+  //printf("%x",RAMDISK_SIZE);
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
